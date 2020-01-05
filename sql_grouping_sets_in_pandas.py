a1f8a67ba727827f7e34959fa9e6159610d5d694
Adam C Bernier <adam.c.bernier@gmail.com>
2020-01-05 11:01:20 -0800

    formatting

--- sql_grouping_sets_in_pandas.py
+++ sql_grouping_sets_in_pandas.py
@@ -12,7 +12,7 @@ def powerset(iterable):
 
 def grouper(df,grpby=None,aggfunc=None):
     ''' produces aggregate DataFrame from DataFrames for non-redundant groupings
-       `workingdf` is used to avoid modifying original DataFrame
+        workingdf is used to avoid modifying original DataFrame
     '''
     uniqcols = (i for i in grpby if len(df[i].unique()) == 1)
     pwrset = (i for i in powerset(grpby))
@@ -20,10 +20,9 @@ def grouper(df,grpby=None,aggfunc=None):
     for uniqcol in uniqcols:
         for i in pwrset:
             if uniqcol in i:
-                s.add(i) # add a level of aggregation only when non-redundant
-
+                # add level of aggregation only when non-redundant
+                s.add(i) 
     workingdf = df.copy()
-    #for idx,i in enumerate(powerset(grpby)):
     for idx,i in enumerate(s):
         print('  grouping by: {}'.format(list(i)))
         if i != (): 
@@ -36,35 +35,27 @@ def grouper(df,grpby=None,aggfunc=None):
             dummycolname = hash(tuple(workingdf.columns.tolist()))
             workingdf[dummycolname] = ''
             tmp = aggfunc( workingdf.groupby(dummycolname) )
-
         # drop the index and add it back
         if i == (): tmp.reset_index(drop=True,inplace=True)
         else: tmp.reset_index(inplace=True)
-
         for j in grpby: 
             if j not in tmp: # if column is not in DataFrame add it
                 tmp[j] = '(All)'
-
         # new list with all columns including aggregate ones; do this only once
         if idx == 0:
             finalcols = grpby[:]
             addlcols = [k for k in tmp if k not in grpby] # aggregate columns 
             finalcols.extend(addlcols)
-
         # reorder columns 
         tmp = tmp[finalcols] 
-
         # creation of final DataFrame
         if idx == 0:
             final = tmp; del tmp
         else: 
             final = pd.concat( [final,tmp] ); del tmp
-
     del workingdf
-
     final = final.sort_values(by=finalcols)
     final.reset_index(drop=True,inplace=True)
-
     return final
 
 def agg(grpbyobj):
@@ -83,7 +74,6 @@ if __name__ == '__main__':
                        'Total':[4,5,6,],})
     final = grouper(df,grpby=['Area','Year'],aggfunc=agg)
     print(final)
-
     # test against expected result 
     expected = '''{"Area":{"0":"(All)","1":"a","2":"b"},
                    "Year":{"0":2014,"1":2014,"2":2014},
